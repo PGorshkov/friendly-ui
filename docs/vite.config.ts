2de01a81e445f86332648138a564db925bdc4380
Pavel Gorshkov <pavelgorshkof@gmail.com>
2023-02-02 00:27:10 +0300

    Настройка github pages

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,10 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    hmr: {
-      overlay: false
-    }
-  },
   base: '/friendly-ui/'
 })
