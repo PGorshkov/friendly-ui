357646770cca55330ad08268e12a1281b2f42451
Pavel Gorshkov <pavelgorshkof@gmail.com>
2023-02-02 09:30:35 +0300

    Обновление стилей для кнопки + обновление конфига

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ module.exports = defineConfig({
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'my-lib',
       formats: ['es'], // adding 'umd' requires globals set to every external module
-      fileName: (format) => `my-lib.${format}.js`,
+      fileName: (format: any) => `my-lib.${format}.js`,
     },
     rollupOptions: {
       // external modules won't be bundled into your library
